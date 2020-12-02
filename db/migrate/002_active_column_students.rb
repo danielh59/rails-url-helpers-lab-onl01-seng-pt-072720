f759b1907631779f1f3fdaa64898e18f7aaa0b66
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-12-02 23:10:56 +0000

    Automatically backed up by Learn

--- db/migrate/002_active_column_students.rb
+++ db/migrate/002_active_column_students.rb
@@ -1,6 +1,6 @@
 class ActiveColumnStudents < ActiveRecord::Migration
 
-def change
-    add_column :students, :active, :boolean, default: false
-end
+  def change
+      add_column :students, :active, :boolean, default: false
+  end
 end
